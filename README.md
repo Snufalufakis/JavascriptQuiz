363257a7b0d69829a0df9070af30fb8d1e9bbf2d
Michael Harris <snufalufakis@gmail.com>
2022-05-24 14:19:02 -0700

    version 2.0

---
+++ README.md
@@ -0,0 +1,94 @@
+
+#JavascriptQuiz
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [User Story](#user-story)
+  - [Acceptance Criteria](#acceptance-criteria)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+
+## Overview
+
+### The challenge
+
+Web APIs Challenge: Code Quiz
+As you proceed in your journey to becoming a full-stack web developer, it’s likely that you’ll be asked to complete a coding assessment, perhaps as part of an interview process. A typical coding assessment is a combination of multiple-choice questions and interactive coding challenges.
+
+To help you become familiar with these tests and give you a chance to apply the skills from this module, this week’s Challenge invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. This week’s coursework will teach you all the skills you need to succeed in this assignment.
+
+### User Story
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+### Acceptance Criteria
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+### Screenshot
+
+![](/screenshot/screenshot-127.0.0.1_5500-2022.05.23-08_30_18.png)
+![](/screenshot/screenshot-127.0.0.1_5500-2022.05.23-12_47_21.png)
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- Javascript
+
+### What I learned
+
+To not let up on studing, took longer than i wanted. to get to this point.
+
+
+### Continued development
+
+Doing JavaScript studies everday is helping the confidence with doing code.
+
+### Useful resources
+
+- [Youtube](https://www.youtube.com/watch?v=f4fB9Xg2JEY&ab_channel=BrianDesign) - This helped me with inspration and some structure.
+- [tutorialspoint](https://www.tutorialspoint.com/javascript/javascript_ifelse.htm) - Helped with correcting my conditional code.
+
+
+## Author
+
+- Twitter - [@Snufalufakis2](https://twitter.com/Snufalufakis2)
+
+
+
+## Acknowledgments
+
+Shout out to freecodecamp for helping me with the basics of JavaScript.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,112 +1,159 @@
-//HTML element refrences
-/*var resultsButton = document.getElementById('results');
-var submitButton = document.getElementById('submit');*/
-var nextButton = document.getElementById("nextButton");
-var myQuestion = document.getElementById("question");
-var myOptions = document.getElementById("options");
+// Html elements
+//Start Section
+const start = document.querySelector("#start");
+const guide = document.querySelector("#guide");
+const exit = document.querySelector("#exit");
+const continueBtn = document.querySelector("#continue");
+const quiz = document.querySelector("#quiz");
+const time = document.querySelector("#time");
+const questionNo = document.querySelector("#questionNo");
+const questionText = document.querySelector("#questionText");
+const choice_que = document.querySelectorAll(".choice_que");
+//Multiple Choices Of Questions
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const option4 = document.querySelector("#option4");
+//correct and next Button
+const total_correct = document.querySelector("#total_correct");
+const next_question = document.querySelector("#next_question");
+//Result Section
+const result = document.querySelector("#result");
+const points = document.querySelector("#points");
+const quit = document.querySelector("#quit");
+const startAgain = document.querySelector("#startAgain");
 
-//Displaing Questions in array
-var qQuestions = [
-  {
-    question: "Which is the type of Pop up box available in JavaScript?",
-    answers: ["Notice", "Announce", "Introduce", "Declare"],
-    correctAnswer: "Notice",
-  },
-  {
-    question: "Which symbol is used for comments in Javascript?",
-    answers: ["//", "-//-", "/*+*/", "**"],
-    correctAnswer: "//",
-  },
-  {
-    question: "Which is a JavaScript Data Type?",
-    answers: ["Boo Diddleys", "Number", "NaN", "Unknown"],
-    correctAnswer: "Number",
-  },
-  {
-    question: "What is the correct place to insert a JavaScript?",
-    answers: [
-      'The "Head" section',
-      'The "Body" section',
-      'Both the "Head" section and the "Body" section',
-      "All of the above",
-    ],
-    correctAnswer: 'Both the "Head" section and the "Body" section'
-  },
-];
-// Functions for displaying Quiz
-var i = 0;
-var len = qQuestions.length;
+//
+let index = 0;
+let timer = 20;
+let interval = 0;
+let UserAns = undefined;
+let correct = 0;
 
-function displayQuiz(i) {
-  
-  // for each question...
-  var question = qQuestions[i].answers.length;
-  for (var a = 0; a < question; a++) {
-    // create choice selectors
-    var option = document.createElement("li");
-    var select = document.createElement("input");
-    select.setAttribute("type", "radio");
-    select.setAttribute("name", "question" + i);
-    option.setAttribute("class", "option");
-    var choice = document.createTextNode(qQuestions[i].answers[a]);
-    
-    myOptions.appendChild(option);
-    option.appendChild(select);
-    option.appendChild(choice);
-    myQuestion.innerHTML = qQuestions[i].question;
-  }
-}
-// For calling displayQuiz
-displayQuiz(0);
+//Start and Exit button actions
+start.addEventListener("click", () => {
+  start.style.display = "none";
+  guide.style.display = "block";
+});
 
-//Button actions
-nextButton.onclick = function () {
-  var correct = 0;
-  var radioList = document.getElementsByName("question" + i);
-  var selected = false;
+exit.addEventListener("click", () => {
+  start.style.display = "block";
+  guide.style.display = "none";
+});
 
-  //check if an answer was selected, if yes, correct
-  for (var b = 0; b < radioList.length; b++) {
-    if (radioList[b].checked) {
-      selected = true;
-      if (b === qQuestions[i].correctAnswer) {
-        correct++;
-      }
-    }
-  }
-  if (!selected) {
-    alert("please select a answer");
-    return undefined;
-  }
-  //
-  while (myOptions.firstChild) {
-    myOptions.removeChild(myOptions.firstChild);
-  }
-  // show next grouping
-  i++;
-  //if it's not the last question, jump to the next one
-  if (i < len) {
-    displayQuiz(i); 
-    
+// Timer for quiz
+function startTimer() {
+  if (timer === 0) {
+    clearInterval(interval);
+    next_question.click();
   } else {
-    quiz.removeChild(nextButton);
-    question.innerHTML = "Results:";
-    var showScore = document.createElement("p");
-    showScore.textContent = "You scored " + correct + "/" + len;
-    quiz.appendChild(showScore);
+    timer--;
+    time.innerHTML = timer;
   }
-  function form() {
-    /*Getting the number of text fields*/
-    var notification = document.getElementById("initials").value;
-    /*Generating text fields dynamically in the same form itself*/
-    for(var i=0;i<notification; i++) {
-        var textfield = document.createElement("input");
-        textfield.type = "text";
-        textfield.value = "Initials here";
-        document.getElementById('form').appendChild(textfield);
-    }
-
 }
-};
+//ToDo: 20 seconds timer, if answer is wrong -5 seconds
 
- 
+function questionData() {
+  questionNo.innerText = index + 1 + ". ";
+  questionText.innerText = qQuestions[index].question;
+  option1.innerText = qQuestions[index].choice1;
+  option2.innerText = qQuestions[index].choice2;
+  option3.innerText = qQuestions[index].choice3;
+  option4.innerText = qQuestions[index].choice4;
+
+  //    timer start
+  timer = 20;
+}
+
+questionData();
+
+// When 'Continue' Button is clicked trigger display change
+continueBtn.addEventListener("click", () => {
+  quiz.style.display = "block";
+  guide.style.display = "none";
+
+  interval = setInterval(startTimer, 1000);
+  questionData();
+
+  choice_que.forEach((removeActive) => {
+    removeActive.classList.remove("active");
+  });
+
+  total_correct.innerHTML = `${(correct = 0)} Out Of ${
+    qQuestions.length
+  } Questions`;
+});
+// selecting a choice will activate correct
+choice_que.forEach((choices, choiceNo) => {
+  choices.addEventListener("click", () => {
+    choices.classList.add("active");
+    //check answer
+    if (choiceNo === qQuestions[index].answer) {
+      correct++;
+    } else {
+      correct += 0;
+    }
+    //stop Counter
+    clearInterval(interval);
+
+    //disable All Options When User Select An Option
+    for (i = 0; i <= 3; i++) {
+      choice_que[i].classList.add("disabled");
+    }
+  });
+});
+
+// If next question is strickly not equal to current question goto next question.
+next_question.addEventListener("click", () => {
+  if (index !== qQuestions.length - 1) {
+    index++;
+    choice_que.forEach((removeActive) => {
+      removeActive.classList.remove("active");
+    });
+
+    questionData();
+
+    //result
+    total_correct.style.display = "block";
+    total_correct.innerHTML = `${correct} Out Of ${qQuestions.length} Questions`;
+    clearInterval(interval);
+    interval = setInterval(startTimer, 1000);
+  } else {
+    index = 0;
+
+    //when Quiz Question Complete Display Result Section
+    clearInterval(interval);
+    quiz.style.display = "none";
+    points.innerHTML = `You Got ${correct} Out Of ${qQuestions.length}`;
+    result.style.display = "block";
+  }
+  for (i = 0; i <= 3; i++) {
+    choice_que[i].classList.remove("disabled");
+  }
+  // store score in localStorage
+  localStorage.setItem(points);
+});
+
+//Initials input
+var saveButton = document.getElementById("save");
+saveButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var nameInput = {
+    initials: nameSave.value,
+  };
+  //store initials in localStorage
+  localStorage.setItem("nameInput", JSON.stringify(nameInput));
+});
+
+//what happen when 'Quit' Button Will Click
+quit.addEventListener("click", () => {
+  start.style.display = "block";
+  result.style.display = "none";
+});
+
+//Start Again clicked
+startAgain.addEventListener("click", () => {
+  guide.style.display = "block";
+  result.style.display = "none";
+});
